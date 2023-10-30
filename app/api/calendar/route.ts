4f277d1c91be55332add4555d559ed6f48d2c2a4
Ernst Luring <ernst@ejldigital.com>
2023-10-30 08:38:38 +0100

    add encoding dev package

--- app/api/calendar/route.ts
+++ app/api/calendar/route.ts
@@ -96,21 +96,19 @@ export async function GET(request: Request) {
     const eventDate = getNextThursday();
     const startDate = formatDateToYMD(eventDate);
 
-    const description = `
-    01 - Get steamy. A warm shower will put your nuts in the mood.
-    02 - Roll one nut between thumb and fingers to check for lumps, swelling or pain.
-    03 - Repeat with the other nut.
-
-    You nuts should feel smooth, firm and sensitive but not painful. 
-    If something doesn't feel right, see a doctor!
-
-    Source: Movember.com
-    https://cdn.movember.com/uploads/files/Your%20Health/TesticularSelfExamination.pdf
-    `;
-
     const newEvent: calendar_v3.Schema$Event = {
       summary: "It's nutchecking time!",
-      description: description,
+      description: `
+      01 - Get steamy. A warm shower will put your nuts in the mood.
+      02 - Roll one nut between thumb and fingers to check for lumps, swelling or pain.
+      03 - Repeat with the other nut.
+  
+      You nuts should feel smooth, firm and sensitive but not painful. 
+      If something doesn't feel right, see a doctor!
+  
+      Source: Movember.com
+      https://cdn.movember.com/uploads/files/Your%20Health/TesticularSelfExamination.pdf
+      `,
       start: {
         date: startDate,
       },
