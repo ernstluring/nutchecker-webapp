957b766c4d35f647988af97eab3681c17676c972
Ernst Luring <ernst@ejldigital.com>
2024-04-15 23:04:01 +0200

    sets runtime as edge

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,8 @@ import { NextRequest, NextResponse } from "next/server";
 // const handler = NextAuth(authOptions);
 // export { handler as GET, handler as POST };
 
+export const runtime = "edge";
+
 interface RouteHandlerContext {
   params: { nextauth: string[] };
 }
